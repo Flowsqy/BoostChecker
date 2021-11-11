25f537c19993025237fe8faa3a6dee3d7c726987
Flowsqy <Flo504.Dev@gmail.com>
2021-11-11 17:20:49 +0100

    Add BoostMessages

---
+++ src/main/java/fr/flowsqy/boostchecker/io/BoostMessages.java
@@ -0,0 +1,27 @@
+package fr.flowsqy.boostchecker.io;
+
+import fr.flowsqy.boostchecker.time.DurationFormatter;
+
+import java.time.Instant;
+
+public class BoostMessages {
+
+    private final String infinite;
+    private final String normal;
+    private final DurationFormatter formatter;
+
+    public BoostMessages(String infinite, String normal, DurationFormatter formatter) {
+        this.infinite = infinite;
+        this.normal = normal;
+        this.formatter = formatter;
+    }
+
+    public String getInfinite() {
+        return infinite;
+    }
+
+    public String getNormal(Instant end){
+        final String duration = formatter.format(end.toEpochMilli() - Instant.now().toEpochMilli());
+        return normal.replace("%duration%", duration);
+    }
+}
