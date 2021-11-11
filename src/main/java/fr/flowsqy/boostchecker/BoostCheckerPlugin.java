dfa7a792fbd4591b0b853a8f6ceabb79cd442608
Flowsqy <Flo504.Dev@gmail.com>
2021-11-11 14:01:11 +0100

    Add project structure

---
+++ src/main/java/fr/flowsqy/boostchecker/BoostCheckerPlugin.java
@@ -0,0 +1,11 @@
+package fr.flowsqy.boostchecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BoostCheckerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
