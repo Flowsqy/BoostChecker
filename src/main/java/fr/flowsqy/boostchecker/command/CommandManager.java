94cab1cffff196101bdc12ccdd5648be4bd10822
Flowsqy <Flo504.Dev@gmail.com>
2021-11-11 18:55:23 +0100

    Link command

--- src/main/java/fr/flowsqy/boostchecker/command/CommandManager.java
+++ src/main/java/fr/flowsqy/boostchecker/command/CommandManager.java
@@ -5,21 +5,27 @@ import fr.flowsqy.boostchecker.task.CheckTask;
 import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
+import java.util.Collections;
 import java.util.Objects;
 
 public class CommandManager {
 
-    private final CheckTask task;
-
     public CommandManager(BoostCheckerPlugin plugin, CheckTask task, YamlConfiguration configuration) {
-        this.task = task;
         final PluginCommand command = plugin.getCommand("bonus");
         Objects.requireNonNull(command);
         final String noPerm = configuration.getString("noperm");
         if(noPerm != null){
             command.setPermissionMessage(ChatColor.translateAlternateColorCodes('&', noPerm));
         }
+        command.setTabCompleter((sender, cmd, label, args) -> Collections.emptyList());
+        command.setExecutor((sender, cmd, label, args) -> {
+            if(sender instanceof Player player){
+                task.perform(player);
+            }
+            return true;
+        });
     }
 
 }
